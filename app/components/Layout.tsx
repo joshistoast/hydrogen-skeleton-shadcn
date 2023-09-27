7a06cef1b8228fde9eea59cfd888d6342b1e160d
Josh Corbett <joshwcorbett@icloud.com>
2023-09-27 09:20:08 -0600

    feat: :fire: remove dead Aside component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -5,7 +5,6 @@ import type {
   FooterQuery,
   HeaderQuery,
 } from 'storefrontapi.generated';
-import {Aside} from '~/components/Aside';
 import {Footer} from '~/components/Footer';
 import {Header, HeaderMenu} from '~/components/Header';
 import {CartMain} from '~/components/Cart';
